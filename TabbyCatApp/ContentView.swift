1a60f2b8bd92d7b70f692a2cc57c43afa4ad83d3
Mousa Sweidan <msweidan@hct.ac.ae>
2023-08-16 10:56:07 +0400

    Initial Commit

---
+++ TabbyCatApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TabbyCatApp
+//
+//  Created by msweidan on 16/08/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TabbyCatApp/TabbyCatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabbyCatAppApp.swift
+//  TabbyCatApp
+//
+//  Created by msweidan on 16/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct TabbyCatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
